8c910624da3c205d70a5c6778c724732ed8357ff
Supun Wimalasena <supun@hyvor.com>
2025-06-07 11:35:38 +0200

    entity / repositories

---
+++ backend/src/Entity/Project.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjectRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProjectRepository::class)]
+#[ORM\Table(name: 'projects')]
+#[ORM\HasLifecycleCallbacks]
+class Project
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'integer', nullable: false)]
+    private int $hyvor_user_id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: false)]
+    private string $name;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private \DateTimeImmutable $created_at;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private \DateTimeImmutable $updated_at;
+
+    public function __construct()
+    {
+        $this->created_at = new \DateTimeImmutable();
+        $this->updated_at = new \DateTimeImmutable();
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getHyvorUserId(): int
+    {
+        return $this->hyvor_user_id;
+    }
+
+    public function setHyvorUserId(int $hyvorUserId): static
+    {
+        $this->hyvor_user_id = $hyvorUserId;
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function getUpdatedAt(): \DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    #[ORM\PreUpdate]
+    public function setUpdatedAt(): void
+    {
+        $this->updated_at = new \DateTimeImmutable();
+    }
+}
