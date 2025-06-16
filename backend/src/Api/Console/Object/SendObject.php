d766fe4261e24034ef41946ee9a3bc064f054e2a
Thibault <thibault.boutet.dev@gmail.com>
2025-06-16 16:00:25 -0400

    Add backend endpoint for listing email and single view

---
+++ backend/src/Api/Console/Object/SendObject.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Api\Console\Object;
+
+use App\Entity\Send;
+
+class SendObject
+{
+    public int $id;
+    public int $created_at;
+    public ?int $sent_at;
+    public ?int $failed_at;
+    public string $status;
+    public string $from_address;
+    public string $to_address;
+    public ?string $subject;
+    public ?string $body_html;
+    public ?string $body_text;
+    public string $raw;
+
+    public function __construct(Send $send)
+    {
+        $this->id = $send->getId();
+        $this->created_at = $send->getCreatedAt()->getTimestamp();
+        $this->sent_at = $send->getSentAt()?->getTimestamp();
+        $this->failed_at = $send->getFailedAt()?->getTimestamp();
+        $this->status = $send->getStatus()->value;
+        $this->from_address = $send->getFromAddress();
+        $this->to_address = $send->getToAddress();
+        $this->subject = $send->getSubject();
+        $this->body_html = $send->getBodyHtml();
+        $this->body_text = $send->getBodyText();
+        $this->raw = $send->getRaw();
+    }
+}
---
+++ backend/src/Api/Console/Resolver/EntityResolver.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Api\Console\Resolver;
+
+use App\Entity\Domain;
+use App\Entity\Project;
+use App\Entity\Send;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class EntityResolver implements ValueResolverInterface
+{
+
+    public const ENTITIES = [
+        'send' => Send::class,
+        'domain' => Domain::class
+    ];
+
+    public function __construct(
+        private EntityManagerInterface $em,
+        private ProjectResolver $projectResolver,
+    ) {
+    }
+
+    /**
+     * @return iterable<mixed>
+     */
+    public function resolve(Request $request, ArgumentMetadata $argument): iterable
+    {
+        $controllerName = $argument->getControllerName();
+        if (!str_starts_with($controllerName, 'App\Api\Console\Controller\\')) {
+            return [];
+        }
+
+        $argumentType = $argument->getType();
+
+        if (!$argumentType || !str_starts_with($argumentType, 'App\Entity\\')) {
+            return [];
+        }
+
+        if ($argumentType === Project::class) {
+            return [];
+        }
+
+        $id = $request->attributes->get('id');
+        $id = is_string($id) ? (int)$id : null;
+
+        if (!$id) {
+            throw new BadRequestException('Invalid ID');
+        }
+
+        $route = $request->getPathInfo();
+        $route = str_replace('/api/console', '', $route);
+
+        $parts = explode('/', $route);
+        $path = $parts[1] ?? null;
+
+        if (!$path) {
+            throw new \Exception('Invalid resource');
+        }
+
+        $entityClass = self::ENTITIES[$path] ?? null;
+
+        if (!$entityClass) {
+            throw new \Exception('Entity for ' . $path . ' not found');
+        }
+
+        $repository = $this->em->getRepository($entityClass);
+        $entity = $repository->find($id);
+
+        if (!$entity) {
+            throw new NotFoundHttpException('Entity not found');
+        }
+
+        $projectOfEntity = $entity->getProject();
+
+        $argumentMetadata = new ArgumentMetadata(
+            'project',
+            Project::class,
+            false,
+            false,
+            null,
+            controllerName: $controllerName
+        );
+        $currentProject = (array)$this->projectResolver->resolve($request, $argumentMetadata);
+        if ($projectOfEntity->getId() !== $currentProject[0]->getId()) {
+            throw new AccessDeniedHttpException('Entity does not belong to the project');
+        }
+
+        return [$entity];
+    }
+
+}
