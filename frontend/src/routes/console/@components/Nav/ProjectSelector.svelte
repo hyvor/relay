14cdee6d4b8e4413ae797cdba78ae174ed376dbc
Thibault <thibault.boutet.dev@gmail.com>
2025-06-17 15:29:20 -0400

    Add project selector

---
+++ frontend/src/routes/console/@components/Nav/ProjectSelector.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { Button, Modal } from '@hyvor/design/components';
+	import { selectingProject } from '../../lib/stores/consoleStore';
+	import IconPlus from '@hyvor/icons/IconPlus';
+	import ProjectList from './ProjectList.svelte';
+</script>
+
+{#if $selectingProject}
+	<Modal size="large" bind:show={$selectingProject}>
+		{#snippet title()}
+			<span class="title">Choose a newsletter </span>
+			<Button as="a" href="/console/new" on:click={() => selectingProject.set(false)}>
+				Create new newsletter
+				{#snippet end()}
+					<IconPlus size={12} />
+				{/snippet}
+			</Button>
+		{/snippet}
+
+		<div class="wrap">
+			<ProjectList />
+		</div>
+	</Modal>
+{/if}
+
+<style>
+	.wrap {
+		padding: 0 15px;
+	}
+
+	.title {
+		font-size: 20px;
+		font-weight: 600;
+		margin-right: 10px;
+	}
+</style>
