1180e826fb4b7c83228e240288ea338b5c017b02
SupunKavinda <supunkavinda1125@gmail.com>
2025-06-19 19:02:29 +0200

    GoState

---
+++ backend/src/Service/Management/GoState/GoState.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Service\Management\GoState;
+
+/**
+ * State of the Go service.
+ */
+class GoState
+{
+
+    public string $hostname;
+
+    /**
+     * Available, active, queue-assigned IP addresses of the current server.
+     * @var GoStateIp[]
+     */
+    public array $ips;
+
+    /**
+     * The number of workers for each IP address.
+     */
+    public int $emailWorkersPerIp;
+
+    /**
+     * The number of workers for the webhook queue (global)
+     */
+    public int $webhookWorkers;
+
+}
---
+++ backend/src/Service/Management/GoState/GoStateIp.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Service\Management\GoState;
+
+class GoStateIp
+{
+
+    // IP address
+    public string $ip;
+
+    // ptr domain (same as EHLO domain)
+    public string $ptr;
+
+    // email queue name to send email from this IP
+    public string $queue;
+
+    // whether the IP should handle incoming emails
+    public bool $incoming;
+}
