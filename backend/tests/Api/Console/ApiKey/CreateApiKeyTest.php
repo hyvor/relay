78489fbe1c30f87216828023acfcb2edc87f0d8d
Thibault <thibault.boutet.dev@gmail.com>
2025-06-19 14:11:11 -0400

    Add creation tests

--- backend/tests/Api/Console/ApiKey/CreateApiKeyTest.php
+++ backend/tests/Api/Console/ApiKey/CreateApiKeyTest.php
@@ -29,4 +29,59 @@ class CreateApiKeyTest extends WebTestCase
         $this->assertArrayHasKey('key', $content);
         $this->assertArrayHasKey('created_at', $content);
     }
+
+    public function test_create_api_key_without_name(): void
+    {
+        $project = ProjectFactory::createOne();
+
+        $response = $this->consoleApi(
+            $project,
+            'POST',
+            '/api-keys',
+            [
+                'scope' => ApiKeyScope::SEND_EMAIL
+            ]
+        );
+
+        $this->assertSame(422, $response->getStatusCode());
+
+        $this->assertHasViolation('name', 'This value should not be blank.');
+    }
+
+    public function test_create_api_key_without_scope(): void
+    {
+        $project = ProjectFactory::createOne();
+
+        $response = $this->consoleApi(
+            $project,
+            'POST',
+            '/api-keys',
+            [
+                'name' => 'Test name'
+            ]
+        );
+
+        $this->assertSame(422, $response->getStatusCode());
+
+        $this->assertHasViolation('scope', 'This value should not be blank.');
+    }
+
+    public function test_create_api_key_invalid_scope(): void
+    {
+        $project = ProjectFactory::createOne();
+
+        $response = $this->consoleApi(
+            $project,
+            'POST',
+            '/api-keys',
+            [
+                'name' => 'Test name',
+                'scope' => 'invalid_scope'
+            ]
+        );
+
+        $this->assertSame(422, $response->getStatusCode());
+
+        $this->assertHasViolation('scope', 'This value should be of type full|send_email.');
+    }
 }
