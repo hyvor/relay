7ccab3d2feecfab3a69e2748179417f26d8c73bc
Thibault <thibault.boutet.dev@gmail.com>
2025-06-24 08:58:55 -0400

    Add migration and entity

--- backend/migrations/Version20250623164704.php
+++ backend/migrations/Version20250623164704.php
@@ -7,34 +7,26 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20250619104621 extends AbstractMigration
+final class Version20250623164704 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return "Create api_keys table";
+        return "Create webhooks table";
     }
 
     public function up(Schema $schema): void
     {
-        $this->addSql(
-            <<<SQL
-            CREATE TYPE scope AS ENUM ('full', 'send_email');
-        SQL
-        );
-
         // Create api_keys table
         $this->addSql(
         <<<SQL
-            CREATE TABLE api_keys (
+            CREATE TABLE webhooks (
                 id SERIAL PRIMARY KEY,
                 created_at TIMESTAMPTZ NOT NULL,
                 updated_at TIMESTAMPTZ NOT NULL,
                 project_id BIGINT NOT NULL references projects(id) ON DELETE CASCADE,
-                key CHAR(64) NOT NULL UNIQUE,
-                name VARCHAR(255) NOT NULL,
-                scope scope NOT NULL DEFAULT 'send_email',
-                is_enabled BOOLEAN NOT NULL DEFAULT TRUE,
-                last_accessed_at TIMESTAMPTZ DEFAULT NULL
+                url VARCHAR(255) NOT NULL,
+                description TEXT NOT NULL,
+                events jsonb NOT NULL,
             );
          SQL
         );
@@ -42,6 +34,6 @@ final class Version20250619104621 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        $this->addSql("DROP TABLE api_keys");
+        $this->addSql("DROP TABLE webhooks");
     }
 }
---
+++ backend/src/Entity/Webhook.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WebhookRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: WebhookRepository::class)]
+class Webhook
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
+
+    #[ORM\Column]
+    private \DateTimeImmutable $created_at;
+
+    #[ORM\Column]
+    private \DateTimeImmutable $updated_at;
+
+    #[ORM\ManyToOne(targetEntity: Project::class)]
+    #[ORM\JoinColumn]
+    private Project $project;
+
+    #[ORM\Column(length: 255)]
+    private string $url;
+
+    #[ORM\Column()]
+    private string $description;
+
+    #[ORM\Column]
+    private array $events = [];
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): \DateTimeImmutable
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updated_at): static
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    public function getProject(): Project
+    {
+        return $this->project;
+    }
+
+    public function setProject(Project $project): static
+    {
+        $this->project = $project;
+
+        return $this;
+    }
+
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): static
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getEvents(): array
+    {
+        return $this->events;
+    }
+
+    public function setEvents(array $events): static
+    {
+        $this->events = $events;
+
+        return $this;
+    }
+}
