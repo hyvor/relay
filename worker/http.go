9e98299dc2c08b4920287d2534a3bb65a53bf4e8
Supun Wimalasena <supun@hyvor.com>
2025-06-26 00:47:06 +0200

    go add some tests

--- worker/http.go
+++ worker/http.go
@@ -77,5 +77,4 @@ func writeJsonResponse(w http.ResponseWriter, data interface{}) {
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
 	}
-
 }
