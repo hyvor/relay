da1acdcf0a1e649cbb723d12a8bf2f1eb556a421
IshiniAvindya <ishiniavindya2000@gmail.com>
2025-06-26 09:38:11 +0200

    wip home
    wip home

--- frontend/src/routes/(marketing)/+page.svelte
+++ frontend/src/routes/(marketing)/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
+<script>
+	import { Button } from "@hyvor/design/components";
+	import Letter from "./Letter.svelte";
+	// import Faq from "./Faq.svelte";
+</script>
+
+<svelte:head>
+	<title>Hyvor Relay</title>
+	<meta name="description" content="Alternative to AWS SES & Mailgun" />
+	<link rel="canonical" href="https://post.hyvor.com" />
+</svelte:head>
+
+<section class="container" id="hero">
+
+	<h1>
+		Hyvor Relay - Self-hosted email sending API
+	</h1>
+
+	<div class="button-wrap">			
+		<Button as="a" href="/relay" size="large">Open</Button>
+	</div>
+
+</section>
+
+<div class="hds-container">
+	<Letter />
+</div>
+
+
+
+<style>
+	#hero {
+		text-align: center;
+		padding: 40px 0 100px;
+	}
+</style>
