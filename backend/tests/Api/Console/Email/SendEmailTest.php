1297fc23fc0a4766310150576b30d6f2b17dce5e
Supun Wimalasena <supun@hyvor.com>
2025-06-30 00:36:54 +0200

    first validation test

--- backend/tests/Api/Console/Email/SendEmailTest.php
+++ backend/tests/Api/Console/Email/SendEmailTest.php
@@ -4,14 +4,55 @@ namespace App\Tests\Api\Console\Email;
 
 use App\Entity\Send;
 use App\Entity\Type\SendStatus;
-use App\Service\Email\Message\EmailSendMessage;
 use App\Tests\Case\WebTestCase;
 use App\Tests\Factory\DomainFactory;
 use App\Tests\Factory\ProjectFactory;
 use App\Tests\Factory\QueueFactory;
+use PHPUnit\Framework\Attributes\TestWith;
 
 class SendEmailTest extends WebTestCase
 {
+
+    /**
+     * @param array<string, mixed> $data
+     */
+    #[TestWith([
+        [
+            'from' => 'invalid email',
+            'to' => 'somebody@example.com',
+            'body_text' => 'test',
+        ],
+        'from',
+        'This value is not a valid email address.'
+    ])]
+    public function test_validation(array $data, string $property, string $violationMessage): void
+    {
+
+        QueueFactory::createTransactional();
+        $project = ProjectFactory::createOne();
+
+        DomainFactory::createOne([
+            'project' => $project,
+            'domain' => 'hyvor.com'
+        ]);
+
+        $this->consoleApi(
+            $project,
+            'POST',
+            '/sends',
+            data: $data
+        );
+
+        $this->assertResponseStatusCodeSame(422);
+
+        $json = $this->getJson();
+        $message = $json['message'];
+        $this->assertIsString($message);
+        $this->assertStringContainsString('Validation failed', $message);
+
+        $this->assertHasViolation($property, $violationMessage);
+
+    }
 
     public function test_queues_mail(): void
     {
