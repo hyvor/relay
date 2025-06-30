840013bd6aaeb498bc9e95e28c6283b1056bd03b
Supun Wimalasena <supun@hyvor.com>
2025-06-30 16:14:07 +0200

    wip

--- backend/tests/Api/Console/AuthorizationTest.php
+++ backend/tests/Api/Console/AuthorizationTest.php
@@ -9,28 +9,29 @@ use PHPUnit\Framework\Attributes\CoversClass;
 #[CoversClass(AuthorizationListener::class)]
 class AuthorizationTest extends WebTestCase
 {
-
     public function test_api_key_authentication_nothing(): void
     {
-        $this->client->request(
-            'GET',
-            '/api/console/sends',
-        );
+        $this->client->request("GET", "/api/console/sends");
         $this->assertResponseStatusCodeSame(403);
-        $this->assertSame('Authorization method not supported. Use either Bearer token or a session.', $this->getJson()['message']);
+        $this->assertSame(
+            "Authorization method not supported. Use either Bearer token or a session.",
+            $this->getJson()["message"]
+        );
     }
 
     public function test_wrong_authorization_header(): void
     {
         $this->client->request(
-            'GET',
-            '/api/console/sends',
+            "GET",
+            "/api/console/sends",
             server: [
-                'HTTP_AUTHORIZATION' => 'WrongHeader',
+                "HTTP_AUTHORIZATION" => "WrongHeader",
             ]
         );
         $this->assertResponseStatusCodeSame(403);
-        $this->assertSame('Authorization header must start with "Bearer ".', $this->getJson()['message']);
+        $this->assertSame(
+            'Authorization header must start with "Bearer ".',
+            $this->getJson()["message"]
+        );
     }
-
 }
