5bdceec9fe4145c59891dabf07696748b12d31df
Supun Wimalasena <supun@hyvor.com>
2025-07-09 21:22:43 +0200

    domain webhook events

--- backend/src/Entity/Type/WebhooksEventEnum.php
+++ backend/src/Entity/Type/WebhooksEventEnum.php
@@ -4,9 +4,18 @@ namespace App\Entity\Type;
 
 enum WebhooksEventEnum: string
 {
+
+    // Created by Workers
     case SEND_DELIVERED = 'send.delivered';
     case SEND_BOUNCED = 'send.bounced';
     case SEND_COMPLAINED = 'send.complained';
+
+    // Created by Workers
     case SUPPRESSION_CREATED = 'suppression.created';
     case SUPPRESSION_DELETED = 'suppression.deleted';
+
+    // Created by API
+    case DOMAIN_CREATED = 'domain.created';
+    case DOMAIN_VERIFIED = 'domain.verified';
+    case DOMAIN_DELETED = 'domain.deleted';
 }
