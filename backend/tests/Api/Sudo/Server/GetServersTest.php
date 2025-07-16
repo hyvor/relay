001b535e35c2bb9a496bdad819c281b44612aa4d
Thibault <88531865+Nolab0@users.noreply.github.com>
2025-07-16 17:44:00 -0400

    94 sudo api authentication (#101)
    * Add sudo listener
    
    * Update sudo tests

--- backend/tests/Api/Sudo/Server/GetServersTest.php
+++ backend/tests/Api/Sudo/Server/GetServersTest.php
@@ -13,27 +13,18 @@ class GetServersTest extends WebTestCase
         // Create test servers
         $server1 = ServerFactory::createOne([
             'hostname' => 'server1.example.com',
-            'api_on' => true,
-            'email_on' => false,
-            'webhook_on' => true,
         ]);
         
         $server2 = ServerFactory::createOne([
             'hostname' => 'server2.example.com',
-            'api_on' => false,
-            'email_on' => true,
-            'webhook_on' => false,
         ]);
         
         $server3 = ServerFactory::createOne([
             'hostname' => 'server3.example.com',
-            'api_on' => true,
-            'email_on' => true,
-            'webhook_on' => true,
         ]);
 
         // Make request to get servers
-        $this->adminApi('GET', '/servers');
+        $this->sudoApi('GET', '/servers');
 
         // Assert response
         $this->assertResponseIsSuccessful();
@@ -51,26 +42,16 @@ class GetServersTest extends WebTestCase
         $this->assertEquals($server1->getId(), $response[0]['id']);
         $this->assertEquals('server1.example.com', $response[0]['hostname']);
         $this->assertEquals($server1->getCreatedAt()->getTimestamp(), $response[0]['created_at']);
-        $this->assertTrue($response[0]['api_on']);
-        $this->assertFalse($response[0]['email_on']);
-        $this->assertTrue($response[0]['webhook_on']);
 
         // Assert server 2 data
         $this->assertEquals($server2->getId(), $response[1]['id']);
         $this->assertEquals('server2.example.com', $response[1]['hostname']);
         $this->assertEquals($server2->getCreatedAt()->getTimestamp(), $response[1]['created_at']);
-        $this->assertFalse($response[1]['api_on']);
-        $this->assertTrue($response[1]['email_on']);
-        $this->assertFalse($response[1]['webhook_on']);
 
         // Assert server 3 data
         $this->assertEquals($server3->getId(), $response[2]['id']);
         $this->assertEquals('server3.example.com', $response[2]['hostname']);
         $this->assertEquals($server3->getCreatedAt()->getTimestamp(), $response[2]['created_at']);
-        $this->assertTrue($response[2]['api_on']);
-        $this->assertTrue($response[2]['email_on']);
-        $this->assertTrue($response[2]['webhook_on']);
-
     }
 
 }
