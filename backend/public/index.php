793e6e2660b287396e90fff4d6dcd888c8f18c5a
Supun Wimalasena <supun@hyvor.com>
2025-07-24 00:25:30 +0200

    wip

--- backend/public/index.php
+++ backend/public/index.php
@@ -5,6 +5,6 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    $env = $context['APP_ENV'];
-    return new Kernel(is_string($env) ? $env : 'dev', (bool) $context['APP_DEBUG']);
+    // @phpstan-ignore-next-line
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
