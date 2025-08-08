7f0ab00f1f6ab2354c6763f9871f843bbf0f75b9
Supun Wimalasena <supun@hyvor.com>
2025-08-08 05:35:10 +0200

    purge stale pending and suspended domains

--- backend/tests/Case/TestSharedTrait.php
+++ backend/tests/Case/TestSharedTrait.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Case;
 
 use App\Service\App\Config;
+use Monolog\Handler\TestHandler;
 use Zenstruck\Foundry\Test\Factories;
 use Zenstruck\Messenger\Test\InteractsWithMessenger;
 
@@ -19,6 +20,13 @@ trait TestSharedTrait
         $reflection = new \ReflectionObject($config);
         $property = $reflection->getProperty($key);
         $property->setValue($config, $value);
+    }
+
+    public function getTestLogger(): TestHandler
+    {
+        $logger = $this->container->get('monolog.handler.test');
+        $this->assertInstanceOf(TestHandler::class, $logger);
+        return $logger;
     }
 
 }
