6dd17c4e27e99a6df6c3c2ddd6c98b55daa55fec
Thibault <thibault.boutet.dev@gmail.com>
2025-08-17 16:28:05 +0200

    Clean private network classes

--- backend/src/Service/PrivateNetwork/PrivateNetworkApi.php
+++ backend/src/Service/PrivateNetwork/PrivateNetworkApi.php
@@ -13,52 +13,9 @@ class PrivateNetworkApi
 {
 
     public function __construct(
-        private HttpClientInterface $httpClient,
         private ServerTaskService $serverTaskService,
     )
     {
-    }
-
-    /**
-     * @throws PrivateNetworkCallException
-     * @return array<mixed>
-     */
-    private function callApiOfServer(
-        Server $server,
-        string $method,
-        string $endpoint, // part after /api/local
-    ): array
-    {
-
-        $privateIp = $server->getPrivateIp();
-
-        if ($privateIp === null) {
-            throw new PrivateNetworkCallException('Server does not have a private IP address assigned.');
-        }
-
-        $endpoint = trim($endpoint, '/');
-        $url = sprintf('http://%s/api/local/%s', $privateIp, $endpoint);
-
-        try {
-            $response = $this->httpClient->request($method, $url);
-            return $response->toArray();
-        } catch (ExceptionInterface $e) {
-
-            $errorContent = 'No content';
-            try {
-                $errorContent = isset($response) ? $response->getContent() : 'No response';
-            } catch (ExceptionInterface) {}
-
-            throw new PrivateNetworkCallException(
-                sprintf(
-                    'Failed to call private network API: %s, Content: %s',
-                    $e->getMessage(),
-                    $errorContent
-                ),
-                previous: $e
-            );
-        }
-
     }
 
     public function callUpdateServerStateApi(Server $server): void
@@ -69,13 +26,4 @@ class PrivateNetworkApi
             []
         );
     }
-
-    /**
-     * @throws PrivateNetworkCallException
-     */
-    public function pingServer(Server $server): void
-    {
-        $this->callApiOfServer($server, 'GET', '/ping');
-    }
-
 }
