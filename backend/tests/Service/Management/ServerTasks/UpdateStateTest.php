f51d0b2b5d63db31aa735e656e04e2623f25e1e3
Thibault <thibault.boutet.dev@gmail.com>
2025-08-19 14:40:15 +0200

    Mock go call

--- backend/tests/Service/Management/ServerTasks/UpdateStateTest.php
+++ backend/tests/Service/Management/ServerTasks/UpdateStateTest.php
@@ -4,14 +4,25 @@ namespace App\Tests\Service\Management\ServerTasks;
 
 use App\Entity\ServerTask;
 use App\Service\Management\Message\ServerTaskMessage;
+use App\Service\Management\MessageHandler\ServerTaskMessageHandler;
 use App\Tests\Case\KernelTestCase;
 use App\Tests\Factory\ServerFactory;
 use App\Tests\Factory\ServerTaskFactory;
+use PHPUnit\Framework\Attributes\CoversClass;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\JsonMockResponse;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
+#[CoversClass(ServerTaskMessage::class)]
+#[CoversClass(ServerTask::class)]
+#[CoversClass(ServerTaskMessageHandler::class)]
 class UpdateStateTest extends KernelTestCase
 {
     public function test_update_state_using_server_tasks(): void
     {
+        $mockResponse = new JsonMockResponse();
+        $this->container->set(HttpClientInterface::class, new MockHttpClient($mockResponse));
+
         $server = ServerFactory::createOne(
             [
                 'hostname' => 'hyvor-relay'
