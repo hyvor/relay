e67f460ab8493fd37a1b3badfd6fc2ca3b91b955
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2025-09-01 16:00:49 +0530

    send.recipient.failed

--- backend/src/Entity/Type/WebhooksEventEnum.php
+++ backend/src/Entity/Type/WebhooksEventEnum.php
@@ -9,6 +9,7 @@ enum WebhooksEventEnum: string
     case SEND_RECIPIENT_DEFERRED = 'send.recipient.deferred';
     case SEND_RECIPIENT_BOUNCED = 'send.recipient.bounced';
     case SEND_RECIPIENT_COMPLAINED = 'send.recipient.complained';
+    case SEND_RECIPIENT_FAILED = 'send.recipient.failed';
 
     // Created by Workers
     case SUPPRESSION_CREATED = 'suppression.created';
