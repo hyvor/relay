13399f4577cef2be925c61bf3cb3b94ec9b810af
SupunKavinda <supunkavinda1125@gmail.com>
2025-09-11 01:24:53 +0530

    sudo OIDC

--- backend/src/Api/Sudo/Authorization/SudoAuthorizationListener.php
+++ backend/src/Api/Sudo/Authorization/SudoAuthorizationListener.php
@@ -3,8 +3,10 @@
 namespace App\Api\Sudo\Authorization;
 
 use Hyvor\Internal\Auth\AuthInterface;
+use Hyvor\Internal\Auth\AuthUser;
 use Hyvor\Internal\Sudo\SudoUserService;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -12,7 +14,8 @@ use Symfony\Component\HttpKernel\KernelEvents;
 #[AsEventListener(event: KernelEvents::CONTROLLER, priority: 200)]
 class SudoAuthorizationListener
 {
-    public const string RESOLVED_SUDO_USER_ATTRIBUTE_KEY = 'sudo_api_resolved_user';
+    private const string RESOLVED_USER_ATTRIBUTE_KEY = 'sudo_api_resolved_user';
+    private const string RESOLVED_SUDO_USER_ATTRIBUTE_KEY = 'sudo_api_resolved_sudo_user';
 
     public function __construct(
         private AuthInterface $auth,
@@ -40,7 +43,14 @@ class SudoAuthorizationListener
             throw new AccessDeniedHttpException('You do not have sudo access.');
         }
 
-        // Store the authenticated user in request attributes
-        $request->attributes->set(self::RESOLVED_SUDO_USER_ATTRIBUTE_KEY, $user);
+        $request->attributes->set(self::RESOLVED_USER_ATTRIBUTE_KEY, $user);
+        $request->attributes->set(self::RESOLVED_SUDO_USER_ATTRIBUTE_KEY, $sudoUser);
+    }
+
+    public static function getUser(Request $request): AuthUser
+    {
+        $user = $request->attributes->get(self::RESOLVED_USER_ATTRIBUTE_KEY);
+        assert($user instanceof AuthUser, 'User must be an instance of AuthUser');
+        return $user;
     }
 } 
