082bb1d00fa508a9958c91b7d181d5e98ec3e8a4
Nadil Karunarathna <113877141+Nadil-K@users.noreply.github.com>
2025-09-12 15:53:25 +0530

    Restrict project creation for cloud (#252)
    * ProjectCreatingEvent
    
    * test
    
    * register only when cloud

---
+++ backend/src/Service/Cloud/ProjectListener.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Service\Cloud;
+
+use App\Api\Console\Authorization\AuthorizationListener;
+use App\Service\App\Config;
+use App\Service\App\HostingEnum;
+use App\Service\Project\Event\ProjectCreatingEvent;
+use Hyvor\Internal\Sudo\SudoUserService;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+#[AsEventListener(event: ProjectCreatingEvent::class, method: 'onProjectCreation')]
+class ProjectListener
+{
+    public function __construct(
+        private RequestStack $requestStack,
+        private SudoUserService $sudoUserService,
+        private Config $config,
+    ) {
+    }
+
+    public function onProjectCreation(ProjectCreatingEvent $event): void
+    {
+        if ($this->config->getHosting() !== HostingEnum::CLOUD) {
+            return;
+        }
+
+        $request = $this->requestStack->getCurrentRequest();
+
+        if ($request === null) {
+            return;
+        }
+
+        if (!str_starts_with($request->getPathInfo(), '/api/console')) {
+            return;
+        }
+
+        if (!AuthorizationListener::hasUser($request)) {
+            return;
+        }
+
+        $user = AuthorizationListener::getUser($request);
+        $isSudo = $this->sudoUserService->exists($user->id);
+
+        if (!$isSudo) {
+            throw new BadRequestHttpException('Currently not available for public usage.');
+        }
+    }
+
+}
