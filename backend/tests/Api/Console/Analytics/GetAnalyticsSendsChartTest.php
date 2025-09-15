7db1935151fd8a9b7ffd531072bb4763e58af446
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2025-09-15 21:33:28 +0530

    analytics

--- backend/tests/Api/Console/Analytics/GetAnalyticsSendsChartTest.php
+++ backend/tests/Api/Console/Analytics/GetAnalyticsSendsChartTest.php
@@ -3,11 +3,12 @@
 namespace App\Tests\Api\Console\Analytics;
 
 use App\Api\Console\Controller\AnalyticsController;
-use App\Entity\Type\SendStatus;
+use App\Entity\Type\SendRecipientStatus;
 use App\Service\Send\SendAnalyticsService;
 use App\Tests\Case\WebTestCase;
 use App\Tests\Factory\ProjectFactory;
 use App\Tests\Factory\SendFactory;
+use App\Tests\Factory\SendRecipientFactory;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Symfony\Component\Clock\Clock;
 use Symfony\Component\Clock\MockClock;
@@ -19,41 +20,40 @@ class GetAnalyticsSendsChartTest extends WebTestCase
 
     public function test_gets_sends_chart_data(): void
     {
-        $this->markTestSkipped();
-        // TODO with new recipient statuses
+        Clock::set(new MockClock('2025-07-14'));
 
-//        Clock::set(new MockClock('2025-07-14'));
-//
-//        $project = ProjectFactory::createOne();
-//
-//        SendFactory::createOne(['project' => $project, 'created_at' => new \DateTimeImmutable('2025-07-01'), 'status' => SendStatus::ACCEPTED]);
-//        SendFactory::createOne(['project' => $project, 'created_at' => new \DateTimeImmutable('2025-07-01'), 'status' => SendStatus::QUEUED]);
-//
-//        SendFactory::createOne(['project' => $project, 'created_at' => new \DateTimeImmutable('2025-07-03'), 'status' => SendStatus::ACCEPTED]);
-//        SendFactory::createOne(['project' => $project, 'created_at' => new \DateTimeImmutable('2025-07-03'), 'status' => SendStatus::ACCEPTED]);
-//        SendFactory::createOne(['project' => $project, 'created_at' => new \DateTimeImmutable('2025-07-03'), 'status' => SendStatus::BOUNCED]);
-//
-//        $this->consoleApi($project, 'GET', '/analytics/sends/chart');
-//
-//        $this->assertResponseStatusCodeSame(200);
-//
-//        /** @var array<array{date: string, accepted: int, bounced: int, queued: int}> $json */
-//        $json = $this->getJson();
-//
-//        $this->assertSame('2025-06-14', $json[0]['date']);
-//        $this->assertSame(0, $json[0]['accepted']);
-//
-//        $july1st = $json[17];
-//        $this->assertSame('2025-07-01', $july1st['date']);
-//        $this->assertSame(1, $july1st['accepted']);
-//        $this->assertSame(0, $july1st['bounced']);
-//        $this->assertSame(1, $july1st['queued']);
-//
-//        $july3rd = $json[19];
-//        $this->assertSame('2025-07-03', $july3rd['date']);
-//        $this->assertSame(2, $july3rd['accepted']);
-//        $this->assertSame(1, $july3rd['bounced']);
-//        $this->assertSame(0, $july3rd['queued']);
+        $project = ProjectFactory::createOne();
+
+        $send1 = SendFactory::createOne(['project' => $project, 'created_at' => new \DateTimeImmutable('2025-07-01')]);
+        SendRecipientFactory::createOne(['send' => $send1, 'status' => SendRecipientStatus::ACCEPTED]);
+        SendRecipientFactory::createOne(['send' => $send1, 'status' => SendRecipientStatus::QUEUED]);
+
+        $send2 = SendFactory::createOne(['project' => $project, 'created_at' => new \DateTimeImmutable('2025-07-03')]);
+        SendRecipientFactory::createOne(['send' => $send2, 'status' => SendRecipientStatus::ACCEPTED]);
+        SendRecipientFactory::createOne(['send' => $send2, 'status' => SendRecipientStatus::ACCEPTED]);
+        SendRecipientFactory::createOne(['send' => $send2, 'status' => SendRecipientStatus::BOUNCED]);
+
+        $this->consoleApi($project, 'GET', '/analytics/sends/chart');
+
+        $this->assertResponseStatusCodeSame(200);
+
+        /** @var array<array{date: string, accepted: int, bounced: int, queued: int}> $json */
+        $json = $this->getJson();
+
+        $this->assertSame('2025-06-14', $json[0]['date']);
+        $this->assertSame(0, $json[0]['accepted']);
+
+        $july1st = $json[17];
+        $this->assertSame('2025-07-01', $july1st['date']);
+        $this->assertSame(1, $july1st['accepted']);
+        $this->assertSame(0, $july1st['bounced']);
+        $this->assertSame(1, $july1st['queued']);
+
+        $july3rd = $json[19];
+        $this->assertSame('2025-07-03', $july3rd['date']);
+        $this->assertSame(2, $july3rd['accepted']);
+        $this->assertSame(1, $july3rd['bounced']);
+        $this->assertSame(0, $july3rd['queued']);
 
     }
 
--- backend/tests/Api/Console/Analytics/GetAnalyticsStatsTest.php
+++ backend/tests/Api/Console/Analytics/GetAnalyticsStatsTest.php
@@ -3,11 +3,13 @@
 namespace App\Tests\Api\Console\Analytics;
 
 use App\Api\Console\Controller\AnalyticsController;
+use App\Entity\Type\SendRecipientStatus;
 use App\Entity\Type\SendStatus;
 use App\Service\Send\SendAnalyticsService;
 use App\Tests\Case\WebTestCase;
 use App\Tests\Factory\ProjectFactory;
 use App\Tests\Factory\SendFactory;
+use App\Tests\Factory\SendRecipientFactory;
 use PHPUnit\Framework\Attributes\CoversClass;
 
 #[CoversClass(AnalyticsController::class)]
@@ -17,26 +19,28 @@ class GetAnalyticsStatsTest extends WebTestCase
 
     public function test_gets_stats(): void
     {
-        $this->markTestSkipped();
-        // TODO with new recipient status
+        $project = ProjectFactory::createOne();
 
-//        $project = ProjectFactory::createOne();
-//
-//        $send1 = SendFactory::createOne(['project' => $project, 'status' => SendStatus::ACCEPTED, 'created_at' => new \DateTime('-10 days')]);
-//        $send2 = SendFactory::createOne(['project' => $project, 'status' => SendStatus::BOUNCED, 'created_at' => new \DateTime('-5 days')]);
-//        $send3 = SendFactory::createOne(['project' => $project, 'status' => SendStatus::COMPLAINED, 'created_at' => new \DateTime('-2 days')]);
-//        $send4 = SendFactory::createOne(['project' => $project, 'status' => SendStatus::QUEUED, 'created_at' => new \DateTime('-1 day')]);
-//
-//        // too old
-//        $send5 = SendFactory::createOne(['project' => $project, 'status' => SendStatus::ACCEPTED, 'created_at' => new \DateTime('-35 day')]);
-//
-//        $this->consoleApi($project, 'GET', '/analytics/stats');
-//        $this->assertResponseStatusCodeSame(200);
-//
-//        $json = $this->getJson();
-//        $this->assertSame(4, $json['sends_30d']);
-//        $this->assertSame(0.25, $json['bounce_rate_30d']);
-//        $this->assertSame(0.25, $json['complaint_rate_30d']);
+        $send1 = SendFactory::createOne(['project' => $project, 'created_at' => new \DateTime('-10 days')]);
+        SendRecipientFactory::createOne(['send' => $send1, 'status' => SendRecipientStatus::ACCEPTED]);
+        $send2 = SendFactory::createOne(['project' => $project, 'created_at' => new \DateTime('-5 days')]);
+        SendRecipientFactory::createOne(['send' => $send2, 'status' => SendRecipientStatus::BOUNCED]);
+        $send3 = SendFactory::createOne(['project' => $project, 'created_at' => new \DateTime('-2 days')]);
+        SendRecipientFactory::createOne(['send' => $send3, 'status' => SendRecipientStatus::COMPLAINED]);
+        $send4 = SendFactory::createOne(['project' => $project, 'created_at' => new \DateTime('-1 day')]);
+        SendRecipientFactory::createOne(['send' => $send4, 'status' => SendRecipientStatus::QUEUED]);
+
+        // too old
+        $send5 = SendFactory::createOne(['project' => $project, 'created_at' => new \DateTime('-35 day')]);
+        SendRecipientFactory::createOne(['send' => $send5, 'status' => SendRecipientStatus::ACCEPTED]);
+
+        $this->consoleApi($project, 'GET', '/analytics/stats');
+        $this->assertResponseStatusCodeSame(200);
+
+        $json = $this->getJson();
+        $this->assertSame(4, $json['sends_30d']);
+        $this->assertSame(0.25, $json['bounce_rate_30d']);
+        $this->assertSame(0.25, $json['complaint_rate_30d']);
 
     }
 
