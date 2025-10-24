8d18c864ac4ada20b7fca5f3dc87c96870852d36
Ishini Senanayake <61535113+IshiniAvindya@users.noreply.github.com>
2025-10-24 09:15:42 +0200

    Homepage: faq, features (#294)
    * faq, features
    
    * pricing
    
    ---------
    
    Co-authored-by: Supun Wimalasena <supun@hyvor.com>

--- frontend/src/routes/(marketing)/+page.svelte
+++ frontend/src/routes/(marketing)/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	import { Button, Tag, Tooltip } from '@hyvor/design/components';
-	import Letter from './Letter.svelte';
+	// import Letter from './Letter.svelte';
+	import FAQ from './FAQ.svelte';
 	import IconCode from '@hyvor/icons/IconCode';
 	import IconHouse from '@hyvor/icons/IconHouse';
 	import Features from './Features.svelte';
+	import Pricing from './Pricing.svelte';
 </script>
 
 <svelte:head>
@@ -13,43 +15,72 @@
 </svelte:head>
 
 <div class="hds-container wrap">
-	<section>
-		<h1>Hyvor Relay</h1>
-		<h2>Email API for Developers</h2>
+	<section class="main-box">
+		<div class="left">
+			<h1>Hyvor Relay</h1>
+			<h2>Email API for Developers</h2>
 
-		<div class="tags">
-			<Tag color="green">
-				{#snippet start()}
-					<IconCode />
-				{/snippet}
-				Open-Source AGPL-3.0
-			</Tag>
-			<Tag color="blue">
-				{#snippet start()}
-					<IconHouse size={12} />
-				{/snippet}
-				Self-Hosted
-			</Tag>
+			<div class="tags">
+				<Tag color="green">
+					{#snippet start()}
+						<IconCode />
+					{/snippet}
+					Open-Source AGPL-3.0
+				</Tag>
+				<Tag color="blue">
+					{#snippet start()}
+						<IconHouse size={12} />
+					{/snippet}
+					Self-Hosted
+				</Tag>
+			</div>
+
+			<div class="button-wrap">
+				<Tooltip
+					text="Cloud public release is planned for 2026. You can self-host Hyvor Relay now."
+				>
+					<Button disabled size="large">Sign up</Button>
+				</Tooltip>
+				<Button as="a" href="/hosting" size="large" variant="outline"
+					>Start Self-Hosting</Button
+				>
+				<Button as="a" href="/docs" size="large" variant="outline">Product Docs</Button>
+			</div>
 		</div>
 
-		<div class="button-wrap">
-			<Tooltip
-				text="Cloud public release is planned for 2026. You can self-host Hyvor Relay now."
-			>
-				<Button disabled size="large">Sign up</Button>
-			</Tooltip>
-			<Button as="a" href="/hosting" size="large" variant="outline">Start Self-Hosting</Button
-			>
-			<Button as="a" href="/docs" size="large" variant="outline">Product Docs</Button>
+		<div class="right">
+			<script async src="https://talk.hyvor.com/embed/newsletter.js" type="module"></script>
+			<hyvor-talk-newsletter website-id="14370"></hyvor-talk-newsletter>
 		</div>
 	</section>
-	<Features />
 	<!-- <Letter /> -->
 </div>
+
+<Pricing />
+<Features />
+<FAQ />
 
 <style>
 	section {
 		padding: 60px 0 100px;
+		display: flex;
+		flex-direction: row;
+		gap: 40px;
+	}
+
+	.left {
+		width: 65%;
+	}
+
+	.left .button-wrap {
+		margin-top: 20px;
+		display: block;
+	}
+	.right {
+		width: 35%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
 	}
 
 	h1 {
@@ -65,10 +96,6 @@
 		margin-top: 12px;
 		margin-bottom: 18px;
 		color: var(--text-light);
-	}
-
-	.wrap {
-		padding-bottom: 100px;
 	}
 
 	.tags {
---
+++ frontend/src/routes/(marketing)/Plan.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+	import { Button } from '@hyvor/design/components';
+
+	interface Props {
+		name: string;
+		price: string;
+		priceSubtext?: string;
+		divider?: boolean;
+		features: string[];
+		ctaText: string;
+		ctaLink: string;
+	}
+
+	let { name, price, priceSubtext, divider, features, ctaText, ctaLink }: Props = $props();
+</script>
+
+<div class="plan" class:divider>
+	<div class="name">{name}</div>
+	<div class="price">{price}</div>
+	<div class="price-subtext">{priceSubtext}</div>
+
+	<div class="features">
+		{#each features as feature}
+			<div class="feature">
+				<span class="mdash">&mdash;</span>{feature}
+			</div>
+		{/each}
+	</div>
+
+	<div class="cta">
+		<Button as="a" variant="outline" href={ctaLink} size="large">{ctaText}</Button>
+	</div>
+</div>
+
+<style>
+	.plan {
+		flex: 1;
+		padding: 30px 20px;
+		position: relative;
+	}
+
+	.name {
+		font-size: 18px;
+		font-weight: 600;
+	}
+
+	.price {
+		font-size: 24px;
+		font-weight: 600;
+		margin-top: 8px;
+	}
+
+	.price-subtext {
+		font-size: 14px;
+		color: var(--text-light);
+		margin-top: 4px;
+	}
+
+	.plan.divider :before {
+		content: '';
+		background-color: var(--border);
+		position: absolute;
+		width: 2px;
+		height: 70%;
+		right: 0;
+		top: 15%;
+	}
+
+	.features {
+		margin-top: 20px;
+		display: flex;
+		flex-direction: column;
+		gap: 6px;
+	}
+
+	.mdash {
+		margin-right: 8px;
+		color: var(--border);
+		user-select: none;
+	}
+
+	.cta {
+		margin-top: 25px;
+		text-align: center;
+	}
+</style>
