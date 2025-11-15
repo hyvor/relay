f25440cc84ab343a076366aed42fc85a52b3e03c
SupunKavinda <supunkavinda1125@gmail.com>
2025-11-15 18:30:38 +0100

    map wip

--- worker/smtp_interface/smtp_interface.go
+++ worker/smtp_interface/smtp_interface.go
@@ -1,4 +1,93 @@
 package smtp_interface
 
+import (
+	"bytes"
+	"errors"
+	"mime"
+	"net/mail"
+)
+
 // converts a SMTP message to a API request compatible with Hyvor Relay's POST /api/console/sends
 
+type ApiRequest struct {
+	From        Address           `json:"from"`
+	To          []Address         `json:"to"`
+	Cc          []Address         `json:"cc,omitempty"`
+	Bcc         []Address         `json:"bcc,omitempty"`
+	Subject     string            `json:"subject"`
+	BodyHtml    string            `json:"body_html,omitempty"`
+	BodyText    string            `json:"body_text,omitempty"`
+	Headers     map[string]string `json:"headers,omitempty"`
+	Attachments []Attachment      `json:"attachments,omitempty"`
+}
+
+type Address struct {
+	Email string `json:"email"`
+	Name  string `json:"name,omitempty"`
+}
+
+type Attachment struct {
+	Content     string `json:"content"` // base64 encoded
+	Name        string `json:"name,omitempty"`
+	ContentType string `json:"content_type,omitempty"`
+}
+
+var ErrNoFromHeader = errors.New("no From header found")
+
+func MimeToApiRequest(mimeMessage []byte) (*ApiRequest, error) {
+
+	message, err := mail.ReadMessage(bytes.NewReader(mimeMessage))
+
+	if err != nil {
+		return nil, err
+	}
+
+	fromAddresses := parseAddressList(message.Header.Get("From"))
+
+	if len(fromAddresses) == 0 {
+		return nil, ErrNoFromHeader
+	}
+
+	mediaType, params, err := mime.ParseMediaType(message.Header.Get("Content-Type"))
+	if err != nil {
+		mediaType = "text/plain"
+	}
+
+	var apiRequest = &ApiRequest{
+		From:    parseAddressList(message.Header.Get("From"))[0],
+		To:      parseAddressList(message.Header.Get("To")),
+		Cc:      parseAddressList(message.Header.Get("Cc")),
+		Bcc:     parseAddressList(message.Header.Get("Bcc")),
+		Subject: message.Header.Get("Subject"),
+		Headers: make(map[string]string),
+	}
+
+	// copy custom headers
+	// TODO:
+
+	// add body
+	// wip:
+
+	//
+
+	return apiRequest, nil
+
+}
+
+func parseAddressList(raw string) []Address {
+
+	if raw == "" {
+		return nil
+	}
+
+	addrs, _ := mail.ParseAddressList(raw)
+	result := make([]Address, 0, len(addrs))
+	for _, a := range addrs {
+		result = append(result, Address{
+			Name:  a.Name,
+			Email: a.Address,
+		})
+	}
+	return result
+
+}
---
+++ worker/smtp_interface/smtp_interface_test.go
@@ -0,0 +1,51 @@
+package smtp_interface
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMimeToApiRequest(t *testing.T) {
+
+	// get all .mime.txt files in the testdata/mapping directory
+	files, err := filepath.Glob("testdata/mapping/*.eml")
+	assert.NoError(t, err)
+
+	for _, file := range files {
+		t.Run(filepath.Base(file), func(t *testing.T) {
+
+			// read the file content
+			content, err := os.ReadFile(file)
+			assert.NoError(t, err)
+
+			errorFile := file[:len(file)-len(".eml")] + ".error"
+
+			// if .error file exists, expect an error
+			if _, err := os.Stat(errorFile); err == nil {
+				expectedError, err := os.ReadFile(errorFile)
+				assert.NoError(t, err)
+
+				_, err = MimeToApiRequest(content)
+				assert.Error(t, err)
+
+				assert.Equal(t, string(expectedError), err.Error())
+			} else {
+				requestFile := file[:len(file)-len(".eml")] + ".json"
+				expectedJson, err := os.ReadFile(requestFile)
+				assert.NoError(t, err)
+
+				apiRequest, err := MimeToApiRequest(content)
+				assert.NoError(t, err)
+
+				apiRequestJson, err := json.MarshalIndent(apiRequest, "", "  ")
+				assert.NoError(t, err)
+
+				assert.JSONEq(t, string(expectedJson), string(apiRequestJson))
+			}
+		})
+	}
+}
