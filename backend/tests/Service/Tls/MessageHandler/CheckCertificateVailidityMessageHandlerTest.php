80f53c6e9a92ecf70ee4b04ff37ba3135375b21c
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2025-12-09 16:16:24 +0530

    revert

--- backend/tests/Service/Tls/MessageHandler/CheckCertificateVailidityMessageHandlerTest.php
+++ backend/tests/Service/Tls/MessageHandler/CheckCertificateVailidityMessageHandlerTest.php
@@ -57,7 +57,7 @@ class CheckCertificateVailidityMessageHandlerTest extends KernelTestCase
 
         $transport = $this->transport(MessageTransport::ASYNC);
         $transport->send($message);
-        $transport->throwExceptions()->process();
+        $transport->throwExceptions()->process(1);
 
        $transport->queue()->assertContains(GenerateCertificateMessage::class);
     }
