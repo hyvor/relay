53a3574f8620f395eef5bd1c51e1aa3c9ee3678b
Thibault <thibault.boutet.dev@gmail.com>
2026-02-09 16:22:06 +0100

    Remove DB call in /health

--- backend/src/Api/Root/RootApiController.php
+++ backend/src/Api/Root/RootApiController.php
@@ -2,23 +2,15 @@
 
 namespace App\Api\Root;
 
-use App\Service\Instance\InstanceService;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RootApiController
 {
 
-    public function __construct(private InstanceService $instanceService)
-    {
-    }
-
     #[Route('/health', methods: ['GET'])]
     public function health(): JsonResponse
     {
-        // to force database connection
-        $this->instanceService->getInstance();
-
         return new JsonResponse(['status' => 'ok']);
     }
 
