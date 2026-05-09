ec4bc9d2f77d0c1013297336b81881fc58e21a01
SupunKavinda <supunkavinda1125@gmail.com>
2026-05-09 10:32:23 +0530

    phpstan

--- backend/src/Service/App/Env/AdvisoryProcessor.php
+++ backend/src/Service/App/Env/AdvisoryProcessor.php
@@ -8,9 +8,15 @@ class AdvisoryProcessor implements EnvVarProcessorInterface
 {
     public function getEnv(string $prefix, string $name, \Closure $getEnv): string
     {
+        /** @var scalar $env */
         $env = $getEnv($name);
-        // replace postgresql:// with postgresql+advisory://
-        return str_replace('postgresql://', 'postgresql+advisory://', $env);
+
+        if (is_string($env) && str_starts_with($env, 'postgresql://')) {
+            // replace postgresql:// with postgresql+advisory://
+            return str_replace('postgresql://', 'postgresql+advisory://', $env);
+        }
+
+        return (string) $env;
     }
 
     public static function getProvidedTypes(): array
